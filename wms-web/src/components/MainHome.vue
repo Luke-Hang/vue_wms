8d44cecfb1b0fad89f60d31278460b8b7280cc28
xiehangz <694851182@qq.com>
2025-06-25 22:47:20 +0800

    添加axiso并进行列表展示

--- wms-web/src/components/MainHome.vue
+++ wms-web/src/components/MainHome.vue
@@ -1,27 +1,56 @@
 <!--  -->
 <template>
       <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140">
+        <el-table-column prop="id" label="ID" width="140">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
+        <el-table-column prop="no" label="账号" width="140">
         </el-table-column>
-        <el-table-column prop="address" label="地址">
+        <el-table-column prop="name" label="姓名" width="140">
+        </el-table-column>
+        <el-table-column prop="age" label="年龄" width="120">
+        </el-table-column>
+        <el-table-column prop="sex" label="性别" width="120">
+        </el-table-column>
+        <el-table-column prop="roleId" label="角色">
+        </el-table-column>
+        <el-table-column prop="phone" label="电话">
+        </el-table-column>
+        <el-table-column prop="op" label="操作">
         </el-table-column>
       </el-table>
 </template>
 
 <script>
 export default {
+  name: "MainHome",
   data () {
-    const item={
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-    };
     return {
-        tableData: Array(10).fill(item)
+        tableData: []
+    }
+  },
+  // Vue 组件的 beforeMount 生命周期钩子中调用了一个名为 loadGet 的方法
+  beforeMount() {
+    this.loadGet();
+    this.loadPost();
+  },
+  methods:{
+    // res=>res.data，取返回数据里面的data字段
+    loadGet(){
+      this.$axios.get(this.$httpUrl+'/user/list').then(res=>res.data).then(res=>{
+        console.log(res)
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    },
+    loadPost(){
+      this.$axios.post(this.$httpUrl+'/user/listP',{}).then(res=>res.data).then(res=>{
+        console.log(res)
+        this.tableData=res.data
+      })
     }
   }
+
 }
 </script>
 
--- wms-web/src/main.js
+++ wms-web/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+Vue.prototype.$httpUrl='http://localhost:8090'
 Vue.use(ElementUI);
 new Vue({
   render: h => h(App),
